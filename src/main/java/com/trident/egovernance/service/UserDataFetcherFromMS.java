d279cd08548ecf869ff06492e253aade0b144113
SwayamCodingEmpire <mohantyswayam2001@gmail.com>
2024-08-23 19:36:22 +0530

    Added webclient requests to fetch access tokens

---
+++ src/main/java/com/trident/egovernance/service/UserDataFetcherFromMS.java
@@ -0,0 +1,11 @@
+package com.trident.egovernance.service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+@Service
+public class ClaimsFetcher {
+
+    public Map<String, Object>
+}
