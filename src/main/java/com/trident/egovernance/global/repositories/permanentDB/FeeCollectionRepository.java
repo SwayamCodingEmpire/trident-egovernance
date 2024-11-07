3074c52a7301377d95c85f767538ab2b90eb8250
SwayamCodingEmpire <mohantyswayam2001@gmail.com>
2024-11-07 21:54:35 +0530

    Session Initiation Complete

--- src/main/java/com/trident/egovernance/global/repositories/permanentDB/FeeCollectionRepository.java
+++ src/main/java/com/trident/egovernance/global/repositories/permanentDB/FeeCollectionRepository.java
@@ -46,9 +46,9 @@ public interface FeeCollectionRepository extends JpaRepository<FeeCollection,Lon
 
     Set<FeeCollection> findAllBySessionId(String sessionId);
 
-    @Query("SELECT f FROM FEECOLLECTION f LEFT JOIN FETCH f.mrNo WHERE f.sessionId = :sessionId AND f.student.regdNo = :regdNo")
-    Set<FeeCollection> findAllBySessionIdAndStudent_RegdNo(String sessionId, String regdNo);
+    @Query("SELECT f FROM FEECOLLECTION f LEFT JOIN FETCH f.student LEFT JOIN FETCH f.mrNo WHERE f.sessionId = :sessionId AND f.student.regdNo = :regdNo")
+    Set<FeeCollection> findAllBySessionIdAndStudent_RegdNoWithAssociations(String sessionId, String regdNo);
 
-    @Query("SELECT f FROM FEECOLLECTION f LEFT JOIN FETCH f.mrNo WHERE f.student.regdNo = :regdNo")
+    @Query("SELECT DISTINCT f FROM FEECOLLECTION f LEFT JOIN FETCH f.student LEFT JOIN FETCH f.mrNo WHERE f.student.regdNo = :regdNo")
     Set<FeeCollection> findAllByRegdNo(String regdNo);
 }
